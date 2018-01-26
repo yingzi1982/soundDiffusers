adabe489741df204e79cd127f05d68d2e0748410
yingzi1982 <yingzi.ying@me.com>
2018-01-26 10:04:09 +0000

    add new files in ...

--- bash/git.sh
+++ bash/git.sh
@@ -2,16 +2,17 @@
 #http://blog.csdn.net/sinat_20177327/article/details/76062030
 #http://kbroman.org/github_tutorial/pages/init.html
 
+operation=$1
 
-
-#create a new repository on the command line
-
-#echo "# temp" >> README.md
-#git init
-#git add README.md
-#git remote add origin https://github.com/yingzi1982/temp.git
-
-git add -u #$uploadFolder
+if [ $operation == 'push' ]
+then
+git add -u
 git commit -m "add new files in $uploadFolder..."
 #git pull origin master
 git push origin master
+elif [ $operation == 'pull' ]
+then
+git reset --hard HEAD
+git clean -f -d
+git pull
+fi

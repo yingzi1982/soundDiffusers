233995f6af0f593c158df1a00a5bff7bbce332ec
yingzi1982 <yingzi.ying@me.com>
2018-02-15 12:56:46 +0000

    pushing ../bash ../figures ../fortran ../gmt ../gnuplot ../latex ../octave ../pbs

--- bash/preprocess_adjoint.sh
+++ bash/preprocess_adjoint.sh
@@ -34,7 +34,7 @@ echo $topoType | ./generateTopography.m
 echo "topography created"
 
 cd ../bash
-#./createPar_file.sh
+./createPar_file.sh
 #-----------------------------------------------------
 
 oldString=`grep "^NSTEP_BETWEEN_OUTPUT_IMAGES " ../DATA/Par_file`
@@ -54,26 +54,7 @@ newString='SAVE_FORWARD                    = .false.'
 sed -i "s/$oldString/$newString/g" ../DATA/Par_file
 
 #--------------------------------------------------------
-runningFolder=../running/$topoType\_$sourceIncidentAngle
-rm -r $runningFolder
-mkdir $runningFolder
-runningOUTPUT_FILESFolder=$runningFolder/OUTPUT_FILES/
-mkdir $runningOUTPUT_FILESFolder
-runningBackupFolder=$runningFolder/backup/
-mkdir $runningBackupFolder
-cp ../backup/sourceTimeFunction ../backup/mesh_info ../backup/source_polar ../backup/source ../backup/receiver ../backup/receiver_polar ../backup/TOMO.xyz  ../backup/topoPolygon $runningBackupFolder
-cp -r ../xmeshfem2D ../xspecfem2D ../DATA/ $runningFolder
-pbsFile=run.pbs
-runningPbsFolder=$runningFolder/pbs/
-mkdir $runningPbsFolder
-cp ../pbs/$pbsFile $runningPbsFolder
-cd $runningPbsFolder
-
-oldString=`grep "^#PBS -N" $pbsFile`
-newString="#PBS -N specfem2d_$topoType\_$sourceIncidentAngle"
-sed -i "s/$oldString/$newString/g" $pbsFile
-
-qsub $pbsFile
+./running.sh $topoType\_$sourceIncidentAngle
 #--------------------------------------------------------
 
 elif [ $simulationType -eq 3 ];

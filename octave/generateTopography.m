02cfd02adcc2bc402859b76fb8a19ae9a78873f2
yingzi1982 <yingzi.ying@me.com>
2018-02-20 09:10:16 +0000

    pushing ../bash/*sh ../figures ../gmt/*sh ../octave/*m ../pbs/*pbs

--- octave/generateTopography.m
+++ octave/generateTopography.m
@@ -64,7 +64,7 @@ topo = interp1(x_sparse,topo_sparse,x,'nearest');
 topo = topo - min(topo);
 case {'gauss','exp', 'von'}
 seed=18;
-correlationLength = 0.05; meanVal=0;stdVal=0.08;
+correlationLength = 0.02; meanVal=0;stdVal=0.08;
 topo=randomField1D(topoType,seed,x,correlationLength,meanVal,stdVal);
 topo = topo - min(topo);
 otherwise
